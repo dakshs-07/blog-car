29faa90fc8ebb2ce407f699ca35f00e17dc0b256
Dakshs-07 <dakshpinjore@gmail.com>
2026-01-18 09:11:05 +0530

    added footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import React from 'react'
+import { FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-blue-200 py-5 items-center">
+      <Image
+        src="/assests/maybach.jpg"
+        width={180}
+        height={200}
+        alt="footer image"
+      />
+      <p className="text-sm">All rights reserved. Copyright @Daksh</p>
+
+      <div className="flex">
+        <a href="https://github.com/dakshs-07">
+          <FaGithub size={30} />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer
