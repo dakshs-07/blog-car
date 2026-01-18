f60bc8fa11322ed6a9934711103da2c69bc6f871
Dakshs-07 <dakshpinjore@gmail.com>
2026-01-18 05:55:43 +0530

    added data file for blog items

---
+++ public/assets/assets.js
@@ -0,0 +1,19 @@
+export const blog_data = [{
+    id: 1, 
+    title: "Learning to drive at a young age",
+    description: "this blog gives you tips on how to drive more comfortably",
+    image: "/assets/maybach.jpg",
+    date: Date.now(),
+    category: "cars",
+    author: "Daksh",
+},
+{
+    id: 1, 
+    title: "Learning to drive at a young age",
+    description: "this blog gives you tips on how to drive more comfortably",
+    image: "/assets/maybach.jpg",
+    date: Date.now(),
+    category: "cars",
+    author: "Daksh",
+},
+]
