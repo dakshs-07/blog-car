a4c3c82931263c0e76aa594d6a1e9c37f092b125
Dakshs-07 <dakshpinjore@gmail.com>
2026-01-18 06:10:59 +0530

    added the functionaloty to filter by category

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
+'use client';
+import BlogList from "@/components/BlogList";
 import Header from "@/components/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
     <Header/>
+    <BlogList />
     </>
   );
 }
--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
-import BlogItem from './BlogItem'
-import { blog_data } from '@/public/assets/assets'
+import React from "react";
+import BlogItem from "./BlogItem";
+import { blog_data } from "@/public/assets/assets";
+import { useState } from "react";
 
 const BlogList = () => {
+  const [menu, setMenu] = useState("All");
+
   return (
     <div>
-      <div className='flex justify-center gap-6 my-10'>
-        <button className='bg-blue-900 text-white py-1 px-4 rounded-sm'>All</button>
-        <button>Cars</button>
-        <button>Driving</button>
-        <button>Technology</button>
+      <div className="flex justify-center gap-6 my-10">
+        <button onClick={()=> setMenu('All')} className={menu==="All"?"bg-blue-900 text-white py-1 px-4 rounded-sm":""}>
+          All
+        </button>
+        <button onClick={()=> setMenu('cars')} className={menu==="cars"?"bg-blue-900 text-white py-1 px-4 rounded-sm":""}>Cars</button>
+        <button onClick={()=> setMenu('driving')} className={menu==="driving"?"bg-blue-900 text-white py-1 px-4 rounded-sm":""}>Driving</button>
+        <button onClick={()=> setMenu('technology')} className={menu==="technology"?"bg-blue-900 text-white py-1 px-4 rounded-sm":""}>Technology</button>
       </div>
-      <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
-        {blog_data.map((item, index)=>{
-            return <BlogItem key={index} title={item.title} description={item.description} category={item.category}/> 
+      <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
+        {blog_data.filter((item)=>menu==="All"?true:item.category===menu).map((item, index) => {
+          return (
+            <BlogItem
+              key={index}
+              title={item.title}
+              description={item.description}
+              category={item.category}
+            />
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogList
+export default BlogList;
