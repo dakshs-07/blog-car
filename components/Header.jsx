39d01fc7b7ba0aa825a74ab56904f7924af8b811
Dakshs-07 <dakshpinjore@gmail.com>
2026-01-18 09:18:22 +0530

    adjusted y padding

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,20 +4,20 @@ import SubscriptionButton from "./SubscriptionButton";
 
 const Header = () => {
   return (
-    <div className="py-5 px-5 md:px-12 lg:px-28">
+    <div className="px-5 py-3 md:px-12 lg:px-28">
       <div className="flex justify-between items-center">
         <Image
           src="/assets/logo.png"
-          height={200}
+          height={100}
           width={180}
-          alt="maybach car lets go"
+          alt="logo"
           className="w-[130] sm:w-auto"
         />
         <button className="flex items-center gap-2 font-medium py-1 px-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#2A52BE] cursor-pointer">
           Go Vroom!
         </button>
       </div>
-      <div className="text-center my-8">
+      <div className="text-center -my-10">
         <h1 className="text-3xl sm:text-5xl font-medium">Latest Blogs</h1>
         <p className="mt-10 max-w-[740] m-auto text-xs sm:text-base">Get all the info you need about top cars, all in one place here.</p>
         <SubscriptionButton />
